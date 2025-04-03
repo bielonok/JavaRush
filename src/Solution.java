9b8c8c60d55151803212d261caf99f35024e6adb
anastasiia <abelonok@gmail.com>
2025-04-03 22:25:16 +0300

    Update conditions

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,8 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Я думаю, бути програмістом - круто");
+        int a = 3126;
+        int b = 8;
+        int c = a - b;
+        System.out.println("Рік народження мого друга це " + c + " piк");
     }
 }
